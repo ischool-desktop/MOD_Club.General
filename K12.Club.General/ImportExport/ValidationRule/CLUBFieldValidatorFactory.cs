2ccaf72902d51e0d0c43ba34f22cdeaefde49ec4
dylan.huang <dylan.huang@ischool.com.tw>
2013-12-26 11:08:39 +0800

    社團模組 - 通用版本(2013/12/26)

---
+++ K12.Club.General/ImportExport/ValidationRule/CLUBFieldValidatorFactory.cs
@@ -0,0 +1,33 @@
+﻿using Campus.DocumentValidator;
+
+namespace K12.Club.General
+{
+    /// <summary>
+    /// 用來產生排課系統所需的自訂驗證規則
+    /// </summary>
+    public class CLUBFieldValidatorFactory : IFieldValidatorFactory
+    {
+        #region IFieldValidatorFactory 成員
+
+        /// <summary>
+        /// 根據typeName建立對應的FieldValidator
+        /// </summary>
+        /// <param name="typeName"></param>
+        /// <param name="validatorDescription"></param>
+        /// <returns></returns>
+        public IFieldValidator CreateFieldValidator(string typeName, System.Xml.XmlElement validatorDescription)
+        {
+            switch (typeName.ToUpper())
+            {
+                case "TEACHERINISCHOOLCHECK":
+                    return new TeacherInischoolCheck(); //取得ischool系統內的所有老師
+                case "DEPTRESTRICTCHECK":
+                    return new DeptRestrictCheck(); //取得ischool系統內的所有科別清單
+                default:
+                    return null;
+            }
+        }
+
+        #endregion
+    }
+}
